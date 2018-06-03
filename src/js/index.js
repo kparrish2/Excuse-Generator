b75a325193dea3757b3e352f2181e8030190365b
KeShaunda <parrish309@gmail.com>
2018-06-03 15:40:07 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,28 @@ import 'bootstrap';
 import '../style/index.scss';
 
 window.onload = function(){
+    
     console.log('Hello Rigo from the console!');
     document.querySelector('.error').style.display = "none";
+    
+    var first = 'A ';
+    var adj = [ 'amazing ', 'marvelous ', 'funky '];
+    var noun = ['rabbit ', 'cat ','ostrich '];
+    var action = ['dropped my ','left my ', 'kicked my '];
+    var possetion= ['car ','keys ', 'purse '];
+    var where = ['in the driveway ','at the circus','at the zoo'];
+    
+    
+    var rdm1 = Math.floor((Math.random() * adj.length) );
+    var rdm2 = Math.floor((Math.random() * adj.length) );
+    var rdm3 = Math.floor((Math.random() * adj.length) );
+    var rdm4 = Math.floor((Math.random() * adj.length) );
+    var rdm5 = Math.floor((Math.random() * adj.length) );
+    
+    
+    var finalSentence = "A "+adj[rdm1]+noun[rdm2]+action[rdm3]+possetion[rdm4]+where[rdm5];
+  
+    document.querySelector("#excuse").innerHTML = finalSentence;
+    
+    
 };
